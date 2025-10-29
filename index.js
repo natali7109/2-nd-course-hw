91e15af385bc80920c4ad69905ab5f8288117745
natali7109 <you@example.com>
2025-10-29 15:48:15 +0300

    ДЗ 2/1 адаптация двух блоков+код

---
+++ index.js
@@ -0,0 +1,51 @@
+let a = 10 ;
+alert (a);
+a = 20;
+alert (a);
+
+let firstIphone = 2007 ;
+alert (firstIphone);
+
+let javascriptCreator = "Brendan Eich";
+alert (javascriptCreator);
+
+let b = 10;
+let c = 2;
+let sum = b+c ;
+alert(sum);
+let difference = b-c ;
+alert (difference);
+let composition = b*c;
+alert (composition);
+let division = b/c ;
+alert (division);
+
+let result = 2 ** 5;
+alert(result);
+
+let d = 9;
+let remainder = d%c;
+alert (remainder);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt("Сколько Вам лет?");
+alert(age);
+
+let user = {
+    name: "Рома",
+    age: 28,
+    isAdmin: true
+};
+alert(user.name + ", " + user.age + " лет, администратор: " + user.isAdmin);
+
+let userName = prompt("Как Вас зовут?");
+alert(`Привет, ${userName}!`);
+
