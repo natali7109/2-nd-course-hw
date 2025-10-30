cb905963e17283deefc718af1d7f89d974b599a1
natali7109 <you@example.com>
2025-10-30 18:44:18 +0300

    ЗД 2/2 устранены недочеты

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 let a = 10 ;
-alert (a);
+alert(a);
 a = 20;
-alert (a);
+alert(a);
 
-let firstIphone = 2007 ;
-alert (firstIphone);
+const firstIphone = 2007 ;
+alert(firstIphone);
 
-let javascriptCreator = "Brendan Eich";
-alert (javascriptCreator);
+const javascriptCreator = "Brendan Eich";
+alert(javascriptCreator);
 
-let b = 10;
-let c = 2;
-let sum = b+c ;
+const b = 10;
+const c = 2;
+let sum = b + c ;
 alert(sum);
-let difference = b-c ;
-alert (difference);
-let composition = b*c;
-alert (composition);
-let division = b/c ;
-alert (division);
+let difference = b - c ;
+alert(difference);
+let composition = b * c;
+alert(composition);
+let division = b / c ;
+alert(division);
 
 let result = 2 ** 5;
 alert(result);
 
-let d = 9;
-let remainder = d%c;
-alert (remainder);
+const d = 9;
+let remainder = d % c;
+alert(remainder);
 
 let num = 1;
 num += 5;
@@ -39,7 +39,7 @@ alert(num);
 let age = prompt("Сколько Вам лет?");
 alert(age);
 
-let user = {
+const user = {
     name: "Рома",
     age: 28,
     isAdmin: true
