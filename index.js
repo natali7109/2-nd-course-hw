178cc11ee482075a74bcaa8fe5f361995d6aa988
natali7109 <you@example.com>
2025-11-04 15:54:13 +0300

    ДЗ 3/1  сделан код js

--- index.js
+++ index.js
@@ -1,51 +1,111 @@
-/*let a = 10 ;
-alert(a);
-a = 20;
-alert(a);
+/*
+let password = "пароль" ;
+let inputPassword = prompt ("Введите пароль");
+inputPassword = inputPassword.toLocaleLowerCase();
+if (inputPassword === password) {
+    alert ("Пароль введен верно");
+} else {
+    alert("Пароль введен неправильно");
+}
 
-const firstIphone = 2007 ;
-alert(firstIphone);
+let c = 3;
+if (c > 0 && c < 10){
+    alert ("Верно");
+} else {
+    alert ("Неверно");
+}
 
-const javascriptCreator = "Brendan Eich";
-alert(javascriptCreator);
+let c = 0;
+if (c > 0 && c < 10){
+    alert ("Верно");
+} else {
+    alert ("Неверно");
+}
 
-const b = 10;
-const c = 2;
-let sum = b + c ;
-alert(sum);
-let difference = b - c ;
-alert(difference);
-let composition = b * c;
-alert(composition);
-let division = b / c ;
-alert(division);
+let c = 10;
+if (c > 0 && c < 10){
+    alert ("Верно");
+} else {
+    alert ("Неверно");
+}
 
-let result = 2 ** 5;
-alert(result);
+let c = -3;
+if (c > 0 && c < 10){
+    alert ("Верно");
+} else {
+    alert ("Неверно");
+}
 
-const d = 9;
-let remainder = d % c;
-alert(remainder);
+let c = 2;
+if (c > 0 && c < 10){
+    alert ("Верно");
+} else {
+    alert ("Неверно");
+}
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
+let d = 50;
+let e = 150;
+if (d > 100 || e > 100) {
+    console.log ("Верно");
+} else {
+    console.log ("Неверно");
+}
 
-let age = prompt("Сколько Вам лет?");
-alert(age);
+let a = "2";
+let b = "3";
+alert (Number(a) + Number(b));
 
-const user = {
-    name: "Рома",
-    age: 28,
-    isAdmin: true
-};
-alert(user.name + ", " + user.age + " лет, администратор: " + user.isAdmin);
 
-let userName = prompt("Как Вас зовут?");
-alert(`Привет, ${userName}!`);*/
+let monthNumber = prompt("Введите номер месяца");
+let month = Number(monthNumber);
+switch (month) {
+    case 12: case 1: case 2:
+        alert("Зима");
+        break;
+    case 3: case 4: case 5:
+        alert("Весна");
+        break;
+    case 6: case 7: case 8:
+        alert("Лето");
+        break;
+    case 9: case 10: case 11:
+        alert("Осень");
+        break;
+    default:
+        alert("Неверный номер месяца");
+}
+
+let input = prompt("Пожалуйста, введите любое число");
+let number = Number(input);
+if (isNaN(number)) {
+    alert("Это не число!");
+} else {
+    if (number % 2 === 0) {
+        alert("Вы ввели: " + number + " (четное число)");
+    } else {
+        alert("Вы ввели: " + number + " (нечетное число)");
+    }
+}
+
+let clientOS;
+if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    clientOS = 0; 
+} else if (/Android/.test(navigator.userAgent)) {
+    clientOS = 1; 
+} else {
+    clientOS = 1; 
+}
+if (clientOS === 0) {
+    console.log ("Установите версию приложения для iOS по ссылке");
+} else {
+    console.log ("Установите версию приложения для Android по ссылке");
+}*/
+
+
+let clientOS = 0; 
+let clientDeviceYear = 2015; 
+let appType = clientDeviceYear >= 2015 ? "" : "облегченную версию ";
+let osName = clientOS === 0 ? "iOS" : "Android";
+let message = `Установите ${appType}приложения для ${osName} по ссылке`;
+console.log(message);
 
